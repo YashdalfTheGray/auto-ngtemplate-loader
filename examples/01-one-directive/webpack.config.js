4a32512d13f83dde8dab74679c340cc0c9d50d53
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-05-28 13:03:56 -0400

    fixed all the paths

--- examples/01-one-directive/webpack.config.js
+++ examples/01-one-directive/webpack.config.js
@@ -3,10 +3,10 @@ const { resolve } = require('path');
 const base = require('../../webpack.base.config');
 
 base.entry = ['./app.js'];
-base.context = resolve('examples/one-directive/src');
+base.context = resolve('examples/01-one-directive/src');
 base.output = {
     filename: 'bundle.js',
-    path: resolve('examples/one-directive/build')
+    path: resolve('examples/01-one-directive/build')
 };
 
 module.exports = base;
