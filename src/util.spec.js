9300936dca3f0ccc4fb23558e6e678d01778fa95
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-01 20:36:00 -0700

    finished out the tests

--- src/util.spec.js
+++ src/util.spec.js
@@ -1,49 +1,13 @@
 const test = require('ava');
-const { findTemplateUrl } = require('./util');
 
-const testService = [
-    `angular.module('test')`,
-    `.service('myService', () => ({`,
-    `\tgetDescription: () => 'test description'`,
-    `}));`
-];
+const { replaceTemplateUrl } = require('./util');
+const { testDirective1, testDirective1Replaced, testDirective2, testDirective2Replaced } = require('./testdata');
 
-const testDirective1 = [
-    `angular.module('test')`,
-    `.directive('myDirective', () => ({`,
-    `\trestrict: 'AE',`,
-    `\tcontroller: 'MyController',`,
-    `\tcontrollerAs: 'MyCtrl',`,
-    `\tbindToController: true,`,
-    `\ttemplateUrl: './myTemplate.html',`,
-    `\tscope: {`,
-    `\t\tvalue: '='`,
-    `\t}`,
-    `}));`
-];
 
-const testDirective2 = [
-    `angular.module('test')`,
-    `.directive('myDirective', () => ({`,
-    `\trestrict: 'AE',`,
-    `\tcontroller: 'MyController',`,
-    `\tcontrollerAs: 'MyCtrl',`,
-    `\tbindToController: true,`,
-    `\tscope: {`,
-    `\t\tvalue: '='`,
-    `\t},`,
-    `\ttemplateUrl:'./anotherTemplate.html'`,
-    `}));`
-];
-
-test('findTemplateUrl returns undefined when there is no templateUrl', (t) => {
-    t.is(findTemplateUrl(testService), undefined);
+test('replaceTemplateUrl returns the modified lines when there is a templateUrl', (t) => {
+    t.deepEqual(replaceTemplateUrl(testDirective1), testDirective1Replaced);
 });
 
-test('findTemplateUrl returns the url when there is a templateUrl', (t) => {
-    t.is(findTemplateUrl(testDirective1), './myTemplate.html');
-});
-
-test('findTemplateUrl returns the url when there is a templateUrl', (t) => {
-    t.is(findTemplateUrl(testDirective2), './anotherTemplate.html');
+test('replaceTemplateUrl returns the modified lines when there is a templateUrl (another case)', (t) => {
+    t.deepEqual(replaceTemplateUrl(testDirective2), testDirective2Replaced);
 });
