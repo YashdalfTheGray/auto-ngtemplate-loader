777a877b8c2a9e44306a10c8b30880fb9857eb35
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-01 20:36:53 -0700

    finished the implementation for replace template

--- src/util.js
+++ src/util.js
@@ -1,7 +1,18 @@
-function findTemplateUrl(lines) {
-    return lines[0];
+function replaceTemplateUrl(lines) {
+    const regEx = /(^\s*templateUrl:\s*)('|")(.*)('|")(,*)$/;
+
+    const lineNumber = lines.reduce((result, line, i) => (/templateUrl/.test(line) ? i : result), -1);
+    const [, , , templateUrl] = regEx.exec(lines[lineNumber]);
+
+    return [
+        `const template = require('ngtemplate-loader!html-loader!${templateUrl}');`,
+        ``,
+        ...lines.slice(0, lineNumber),
+        lines[lineNumber].replace(regEx, '$1template$5'),
+        ...lines.slice(lineNumber + 1)
+    ];
 }
 
 module.exports = {
-    findTemplateUrl
+    replaceTemplateUrl
 };
