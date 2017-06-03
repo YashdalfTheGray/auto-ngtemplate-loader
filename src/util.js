67c696d5b771cdb609ca70d2721d0e4968e66e50
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-02 19:33:29 -0700

    less capture groups means better regex

--- src/util.js
+++ src/util.js
@@ -1,14 +1,14 @@
 function replaceTemplateUrl(lines) {
-    const regEx = /(^\s*templateUrl:\s*)('|")(.*)('|")(,*)$/;
+    const regEx = /(^\s*templateUrl:\s*)['"](.*)['"](,*)$/;
 
     const lineNumber = lines.reduce((result, line, i) => (/templateUrl/.test(line) ? i : result), -1);
-    const [, , , templateUrl] = regEx.exec(lines[lineNumber]);
+    const [, , templateUrl] = regEx.exec(lines[lineNumber]);
 
     return [
-        `const template = require('ngtemplate-loader!html-loader!${templateUrl}');`,
+        `const template = require('${templateUrl}');`,
         ``,
         ...lines.slice(0, lineNumber),
-        lines[lineNumber].replace(regEx, '$1template$5'),
+        lines[lineNumber].replace(regEx, '$1template$3'),
         ...lines.slice(lineNumber + 1)
     ];
 }
