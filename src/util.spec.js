70560affcfb1441649763195d9a99e531fcca707
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-02 20:13:47 -0700

    added a test to return a file as it is

--- src/util.spec.js
+++ src/util.spec.js
@@ -2,12 +2,17 @@ const test = require('ava');
 
 const { replaceTemplateUrl } = require('./util');
 const {
+    testService,
     testDirective1,
     testDirective1Replaced,
     testDirective2,
     testDirective2Replaced
 } = require('./testdata');
 
+
+test('replaceTemplateUrl returns the file if there is not a template', (t) => {
+    t.deepEqual(replaceTemplateUrl(testService.split('\n')), testService.split('\n'));
+});
 
 test('replaceTemplateUrl returns the modified lines when there is a templateUrl', (t) => {
     t.deepEqual(replaceTemplateUrl(testDirective1.split('\n')), testDirective1Replaced.split('\n'));
