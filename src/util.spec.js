e71a3195d96aeefc7ea370f62fc9a9ec911a9e88
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-02 19:32:59 -0700

    started using the refactored test data

--- src/util.spec.js
+++ src/util.spec.js
@@ -1,13 +1,18 @@
 const test = require('ava');
 
 const { replaceTemplateUrl } = require('./util');
-const { testDirective1, testDirective1Replaced, testDirective2, testDirective2Replaced } = require('./testdata');
+const {
+    testDirective1,
+    testDirective1Replaced,
+    testDirective2,
+    testDirective2Replaced
+} = require('./testdata');
 
 
 test('replaceTemplateUrl returns the modified lines when there is a templateUrl', (t) => {
-    t.deepEqual(replaceTemplateUrl(testDirective1), testDirective1Replaced);
+    t.deepEqual(replaceTemplateUrl(testDirective1.split('\n')), testDirective1Replaced.split('\n'));
 });
 
 test('replaceTemplateUrl returns the modified lines when there is a templateUrl (another case)', (t) => {
-    t.deepEqual(replaceTemplateUrl(testDirective2), testDirective2Replaced);
+    t.deepEqual(replaceTemplateUrl(testDirective2.split('\n')), testDirective2Replaced.split('\n'));
 });
