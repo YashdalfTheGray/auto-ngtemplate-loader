d1797ff30832b2db3c2fed37caf5c7e6eb9b7f35
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-11 18:49:15 -0700

    wiring for variable name change

--- src/auto-ngtemplate-loader.js
+++ src/auto-ngtemplate-loader.js
@@ -1,12 +1,23 @@
+const loaderUtils = require('loader-utils');
+const { escapeRegExp } = require('lodash');
+const { isValid } = require('var-validator');
+
 const { replaceTemplateUrl } = require('./util');
 
 module.exports = function autoNgTemplateLoader(source, map) {
-    if (/^const autoNgTemplateLoaderTemplate/.test(source)) {
+    const { variableName = 'autoNgTemplateLoaderTemplate' } = loaderUtils.getOptions(this) || {};
+
+    if (!isValid(variableName)) {
+        this.callback(new Error('Specified variable name is not valid'), null, null);
+        return;
+    }
+
+    if (new RegExp(`^const ${escapeRegExp(variableName)}`).test(source)) {
         this.callback(null, source, map);
         return;
     }
 
-    const newSource = replaceTemplateUrl('autoNgTemplateLoaderTemplate', source.split('\n')).join('\n');
+    const newSource = replaceTemplateUrl(variableName, source.split('\n')).join('\n');
 
     this.callback(null, newSource, map);
 };
