98fc1f1db7fd25c5de89b3d70a4f7a6cf1a032f3
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-12 21:38:29 -0700

    added more tests around taking in a query object or string

--- src/auto-ngtemplate-loader.spec.js
+++ src/auto-ngtemplate-loader.spec.js
@@ -15,3 +15,49 @@ test('loader works on files that need templates replaced', (t) => {
 
     loader.call(context, testDirective1);
 });
+
+test('loader works with an query object', (t) => {
+    t.plan(2);
+
+    const context = {
+        callback: function callback(err, source) {
+            t.is(err, null);
+            t.deepEqual(source, testDirective1Replaced('testVariable'));
+        },
+        query: {
+            variableName: 'testVariable'
+        }
+    };
+
+    loader.call(context, testDirective1);
+});
+
+test('loader works with an query string', (t) => {
+    t.plan(2);
+
+    const context = {
+        callback: function callback(err, source) {
+            t.is(err, null);
+            t.deepEqual(source, testDirective1Replaced('testVariable'));
+        },
+        query: '?variableName=testVariable'
+    };
+
+    loader.call(context, testDirective1);
+});
+
+test('loader throws an error when variable is not valid', (t) => {
+    t.plan(2);
+
+    const context = {
+        callback: function callback(err, source) {
+            t.deepEqual(err, new Error('Specified variable name is not valid'));
+            t.is(source, null);
+        },
+        query: {
+            variableName: 'test variable'
+        }
+    };
+
+    loader.call(context, testDirective1);
+});
