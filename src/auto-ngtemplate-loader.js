007771802ae0877629a3632e31604800a6ba0581
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-18 14:07:27 -0800

    added a way to get the function from the config if webpack v1

--- src/auto-ngtemplate-loader.js
+++ src/auto-ngtemplate-loader.js
@@ -1,19 +1,44 @@
 const loaderUtils = require('loader-utils');
 const { escapeRegExp } = require('lodash');
 const { isValid } = require('var-validator');
+const _ = require('lodash');
 
 const { replaceTemplateUrl } = require('./util');
 
 module.exports = function autoNgTemplateLoader(source, map) {
+    let resolverFunc;
+    let resolverFromConfig;
     const {
         variableName = 'autoNgTemplateLoaderTemplate',
         pathResolver,
         useResolverFromConfig = false
     } = loaderUtils.getOptions(this) || {};
 
-    if (useResolverFromConfig && this.version > 1) {
-        this.callback(new Error('Resolver required to be passed as an option with Webpack v2'), null, null);
-        return;
+    if (useResolverFromConfig) {
+        if (this.version > 1) {
+            this.callback(
+                new Error('Resolver required to be passed as an option with Webpack v2'),
+                null,
+                null
+            );
+            return;
+        }
+
+        resolverFromConfig = _.get(this.options.autoNgTemplateLoader.pathResolver);
+
+        if (!resolverFromConfig) {
+            this.callback(
+                new Error('function pathResolver not found in autoNgTemplateLoader in the config'),
+                null,
+                null
+            );
+            return;
+        }
+
+        resolverFunc = resolverFromConfig;
+    }
+    else {
+        resolverFunc = pathResolver;
     }
 
     if (!isValid(variableName)) {
@@ -27,7 +52,7 @@ module.exports = function autoNgTemplateLoader(source, map) {
     }
 
     try {
-        const newSource = replaceTemplateUrl(variableName, source.split('\n'), pathResolver).join('\n');
+        const newSource = replaceTemplateUrl(variableName, source.split('\n'), resolverFunc).join('\n');
         this.callback(null, newSource, map);
     }
     catch (e) {
